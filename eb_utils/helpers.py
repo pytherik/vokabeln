3df5151ae7098d8e668e74552673a385fd9d4d32
pytherik <erikberndt@gmx.net>
2022-10-07 23:11:24 +0200

    add clear function to helpers.py, some f-string changes

--- eb_utils/helpers.py
+++ eb_utils/helpers.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
+import sys
+from os import system
 
 class Colors:
 
-    R="\033[1;31m"
-    G="\033[1;32m"
-    Y="\033[1;33m"
-    B="\033[0;34m"
-    P="\033[0;35m"
-    LB="\033[1;34m"
-    E="\033[00m"
+    R  = "\033[1;31m"
+    G  = "\033[1;32m"
+    Y  = "\033[1;33m"
+    BR = "\033[33m"
+    B  = "\033[0;34m"
+    P  = "\033[0;35m"
+    LB = "\033[1;34m"
+    E  = "\033[00m"
+
+def clear():
+    os = sys.platform
+    if 'linux' in os:
+        system('clear')
+    elif 'win' in os:
+        system('cls')
+        
